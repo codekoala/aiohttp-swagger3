9d94cf9949951a7ae90e979d9d9e268ee58971f1
Josh VanderLinden <josh@fyle.com>
2019-04-03 14:50:09 -0400

    Avoid import errors during install

--- aiohttp_swagger3/__init__.py
+++ aiohttp_swagger3/__init__.py
@@ -2,5 +2,8 @@ __all__ = ("SwaggerDocs", "SwaggerFile", "__version__")
 __version__ = "0.1.5"
 __author__ = "Valetov Konstantin"
 
-from .swagger_docs import SwaggerDocs
-from .swagger_file import SwaggerFile
+try:
+    from .swagger_docs import SwaggerDocs
+    from .swagger_file import SwaggerFile
+except ImportError:
+    pass
